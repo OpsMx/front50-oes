1437b8ee7c39817f75915cac5b272c7826e0175f
aman-agrawal <9412470@gmail,com>
2023-07-13 12:40:06 +0530

    fix

--- front50-web/src/main/java/com/netflix/spinnaker/front50/Main.java
+++ front50-web/src/main/java/com/netflix/spinnaker/front50/Main.java
@@ -39,13 +39,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
       DataSourceAutoConfiguration.class,
       GsonAutoConfiguration.class
     })
-@ComponentScan({
-  "com.netflix.spinnaker.front50",
-  "com.netflix.spinnaker.config",
-  "com.netflix.spinnaker.kork",
-  "com.netflix.spectator.api",
-  "com.netflix.spinnaker.front50.config"
-})
+@ComponentScan({"com.netflix.spinnaker.front50", "com.netflix.spinnaker.config"})
 public class Main extends SpringBootServletInitializer {
 
   private static final Map<String, Object> DEFAULT_PROPS =
